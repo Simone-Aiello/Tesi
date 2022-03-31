1e1986d28d480bf0f7c993b88dc5e0b529e2a8e8
Simone-Aiello <simoneaiello2000@outlook.com>
2022-04-01 00:03:19 +0200

    confusion matrix and svm beginning

--- Machine Learning/IsolationForest.py
+++ Machine Learning/IsolationForest.py
@@ -1,158 +1,241 @@
+from math import gamma
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.dates as dates
 import numpy as np
-from random import  randrange
 import datetime
 from sklearn.ensemble import IsolationForest
+from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from mlxtend.plotting import plot_decision_regions
+import seaborn as sns
+def train_isolation_forest(x_train,x_test,y_train,y_test):
+    contamination = 0.06
+    model = IsolationForest(contamination=contamination, max_features=x_train.shape[1])
 
-def random_date(start,l):
-    dates = []
-    current = start
-    while l > 0:
-        current = current + datetime.timedelta(minutes=randrange(300))
-        while current in dates:
-            current = current + datetime.timedelta(minutes=randrange(300))
-        dates.append(current)
-        l-=1
-    return dates
+    #Training the model with train data
+    model.fit(x_train.to_numpy())
+    anomaly_score_training = model.decision_function(x_train.to_numpy())
+    training_prediction = model.predict(x_train.to_numpy())
 
-#pre-processing speed data
-speed_df = pd.read_csv('speed.csv')
-speed_df = speed_df.drop(['id', 'vehicle_id','sensor'], axis=1)
-speed_df = speed_df.rename(columns={"data" : "speed"})
 
-#pre-processing rpm data
-rpm_df = pd.read_csv('rpm.csv')
-rpm_df = rpm_df.drop(['id', 'vehicle_id','sensor'], axis=1)
-rpm_df = rpm_df.rename(columns={"data" : "rpm"})
+    #Plotting training heatmap
+    anomaly_train_plot = plt.scatter(x_train['rpm'], x_train['speed'], c = -1*anomaly_score_training, cmap = 'coolwarm')
+    plt.colorbar(anomaly_train_plot,label = 'More Red = More Anomalous')
+    plt.xlabel('RPM', fontsize = 12)
+    plt.ylabel('Speed', fontsize = 12)
+    plt.grid()
+    plt.savefig(f"heatmap_train_contamination_{contamination}.png")
+    plt.clf()
+
+    #Plotting training prediction
+    prediction_plot = plt.scatter(x_train['rpm'], x_train['speed'], c = -1*training_prediction, cmap = 'coolwarm')
+    plt.colorbar(prediction_plot,label = 'More Red = More Anomalous')
+    plt.xlabel('RPM', fontsize = 14)
+    plt.ylabel('Speed', fontsize = 14)
+    plt.grid()
+    plt.title(f'Contamination = {contamination}', weight = 'bold')
+    plt.savefig(f"prediction_train_contamination_{contamination}.png")
+    plt.clf()
+
+    #Testing the accuracy of the model on data unseen before
+    anomaly_score_test = model.decision_function(x_test.to_numpy())
+    test_prediction = model.predict(x_test.to_numpy())
+
+    #Plotting test heatmap
+    anomaly_test_plot = plt.scatter(x_test['rpm'], x_test['speed'], c = -1*anomaly_score_test, cmap = 'coolwarm')
+    plt.colorbar(anomaly_test_plot,label = 'More Red = More Anomalous')
+    plt.xlabel('RPM', fontsize = 12)
+    plt.ylabel('Speed', fontsize = 12)
+    plt.grid()
+    plt.savefig(f"heatmap_test_contamination_{contamination}.png")
+    plt.clf()
+
+    #Plotting test prediction
+    test_prediction_plot = plt.scatter(x_test['rpm'], x_test['speed'], c = -1*test_prediction, cmap = 'coolwarm')
+    plt.colorbar(test_prediction_plot,label = 'More Red = More Anomalous')
+    plt.xlabel('RPM', fontsize = 14)
+    plt.ylabel('Speed', fontsize = 14)
+    plt.grid()
+    plt.title(f'Contamination = {contamination}', weight = 'bold')
+    plt.savefig(f"prediction_test_contamination_{contamination}.png")
+    plt.clf()
+
+    #Evaluating accuracy of the model
+
+    #In isolation forest -1 is anomalous and 1 is not anomalous
+    training_prediction = [0 if i == 1 else 1 for i in training_prediction]
+    test_prediction = [0 if i == 1 else 1 for i in test_prediction]
+
+
+    #Plotting the result
+    columns = ["Predicted not anomalous","Predicted anomalous"]
+    rows = ["Actual not anomalous","Actual anomalous"]
+    training_confusion_matrix = pd.DataFrame(confusion_matrix(y_true=y_train,y_pred=training_prediction),columns=columns,index=rows)
+    sns.heatmap(training_confusion_matrix,annot=True,fmt='d',cmap="gist_gray_r",cbar=False).set(title="Training result")
+    plt.savefig(f"training_confusion_matrix_{contamination}.png")
+    plt.clf()
+    testing_confusion_matrix = pd.DataFrame(confusion_matrix(y_true=y_test,y_pred=test_prediction),columns=columns,index=rows)
+    sns.heatmap(testing_confusion_matrix,annot=True,fmt='d',cmap="gist_gray_r",cbar=False).set(title="Testing result")
+    plt.savefig(f"testing_confusion_matrix_{contamination}.png")
+def train_svc(x_train,x_test,y_train,y_test):
+    gamma = 0.00001 #0.001 o questo sono quelli che vanno meglio, capire perché magari facendo il plot della regione che prendono
+    svm = SVC(kernel="rbf",gamma=gamma,random_state=3)
+    svm.fit(x_train,y_train)
+    hyperplane_distance = svm.decision_function(x_test)
+    prediction = svm.predict(x_test)
+    hyperplane_distance_plot = plt.scatter(x_test['rpm'], x_test['speed'], c = hyperplane_distance, cmap = 'coolwarm')
+    plt.colorbar(hyperplane_distance_plot,label = 'Distance from the hyperplane')
+    plt.xlabel('RPM', fontsize = 12)
+    plt.ylabel('Speed', fontsize = 12)
+    plt.grid()
+    plt.savefig(f"heatmap_train_gamma_{gamma}.png")
+    plt.clf()
+    
+    hyperplane_prediction_plot = plt.scatter(x_test['rpm'], x_test['speed'], c = prediction, cmap = 'coolwarm')
+    plt.colorbar(hyperplane_prediction_plot,label = 'Predictions')
+    plt.xlabel('RPM', fontsize = 12)
+    plt.ylabel('Speed', fontsize = 12)
+    plt.grid()
+    plt.savefig(f"prediction_train_gamma_{gamma}.png")
+    plt.clf()
+
+    plot_decision_regions(x_train.to_numpy(), y_train.to_numpy(), clf=svm, legend=2)
+    plt.savefig("decision_regions.png")
+
+#Plotting starting dataset
+starting_speed = pd.read_csv("speed.csv")
+starting_rpm = pd.read_csv("rpm.csv")
+ax = plt.gca()
+ax.set_xlim([-100, 4000])
+plt.xlabel('RPM', fontsize = 12)
+plt.ylabel('Speed', fontsize = 12)
+plt.scatter(starting_rpm["data"],starting_speed["data"],c=[0]*starting_speed.shape[0],cmap="coolwarm")
+plt.savefig("starting_rpm_speed_scatter.png")
+plt.clf()
+
+#pre-processing speed data containing anomalies
+speed_df = pd.read_csv("speedAn.csv")
+speed_df = speed_df.drop(["id"], axis=1)
+
+#pre-processing rpm dataset containing anomalies
+rpm_df = pd.read_csv("rpmAn.csv")
+rpm_df = rpm_df.drop(["id","anomaly"], axis=1)
 
 #joining rpm and speed dataframe on timestamp
-columns_names = ["timestamp","speed","rpm"]
+columns_names = ["timestamp","speed","rpm","is_anomalous"]
 rpm_speed_df = pd.merge(rpm_df,speed_df,on="timestamp")
+rpm_speed_df = rpm_speed_df.rename(columns={"anomaly" : "is_anomalous"})
 rpm_speed_df = rpm_speed_df.reindex(columns=columns_names)
-label_df = pd.DataFrame({"anomaly" : [-1]*rpm_speed_df.shape[0]})
-rpm_speed_df = pd.concat([rpm_speed_df,label_df],axis=1)
-ax = plt.gca()
-ax.set_xlim([-100, 4500])
-plt.scatter(rpm_speed_df["rpm"],rpm_speed_df["speed"],c=rpm_speed_df["anomaly"],cmap="coolwarm")
-plt.savefig("rpm_speed_scatter_1.png")
+#ax.set_xlim([-100, 4000])
+
+#Plotting dataset containing anomalies
+plt.xlabel('RPM', fontsize = 12)
+plt.ylabel('Speed', fontsize = 12)
+plt.scatter(rpm_speed_df["rpm"],rpm_speed_df["speed"],c=rpm_speed_df["is_anomalous"],cmap="coolwarm")
+plt.savefig("rpm_speed_scatter.png")
 plt.clf()
 
-#inserting anomalies
-rng = np.random.default_rng()
-low_speed_df = pd.DataFrame(rng.integers(0, 40, size=(75, 1)), columns=['speed']) #low speed values
-high_speed_df = pd.DataFrame(rng.integers(80, 150, size=(75, 1)), columns=['speed']) #high speed values
 
-low_rpm_df = pd.DataFrame(rng.integers(0, 1000, size=(75, 1)), columns=['rpm']) #low rpm values
-high_rpm_df = pd.DataFrame(rng.integers(2500, 4000, size=(75, 1)), columns=['rpm']) #high rpm values
-
-today_date = datetime.datetime.now()
-first_random_date_df = []
-second_random_date_df = []
-
-for x in random_date(today_date,75):
-    first_random_date_df.append(x.strftime("%Y-%m-%d %H:%M:%S"))
-for x in random_date(today_date + datetime.timedelta(days=15),75):
-    second_random_date_df.append(x.strftime("%Y-%m-%d %H:%M:%S"))
-
-first_anomalies_df = pd.DataFrame({"timestamp": first_random_date_df,"speed": low_speed_df["speed"],"rpm":high_rpm_df["rpm"],"anomaly":[1]*75})
-second_anomalies_df = pd.DataFrame({"timestamp": second_random_date_df,"speed": high_speed_df["speed"],"rpm":low_rpm_df["rpm"],"anomaly":[1]*75})
-rpm_speed_df = pd.concat([rpm_speed_df,first_anomalies_df,second_anomalies_df],ignore_index=True)
-plt.scatter(rpm_speed_df["rpm"],rpm_speed_df["speed"],c=rpm_speed_df["anomaly"],cmap='coolwarm')
-plt.savefig("rpm_speed_anomaly_scatter_1.png")
-plt.clf()
-
-#Creating dataframe for training and testing
-labels = rpm_speed_df["anomaly"]
-complete_dataframe = rpm_speed_df.drop(["timestamp","anomaly"],axis=1)
-x_train, x_test, y_train, y_test = train_test_split(complete_dataframe,labels,test_size=0.3,stratify=labels)
+#Splitting dataframe for training and testing
+labels = rpm_speed_df["is_anomalous"]
+complete_dataframe = rpm_speed_df.drop(["timestamp","is_anomalous"],axis=1)
+x_train, x_test, y_train, y_test = train_test_split(complete_dataframe,labels,test_size=0.3,stratify=labels,random_state=3)
 
 #Plotting training data
+plt.xlabel('RPM', fontsize = 12)
+plt.ylabel('Speed', fontsize = 12)
 plt.scatter(x_train["rpm"],x_train["speed"],c=y_train,cmap='coolwarm')
 plt.savefig("train_scatter")
 plt.clf()
 
 #Plotting testing data
+plt.xlabel('RPM', fontsize = 12)
+plt.ylabel('Speed', fontsize = 12)
 plt.scatter(x_test["rpm"],x_test["speed"],c=y_test,cmap='coolwarm')
 plt.savefig("test_scatter")
 plt.clf()
 
+#train_isolation_forest(x_train,x_test,y_train,y_test)
+train_svc(x_train,x_test,y_train,y_test)
 #Machine learning model: Isolation Forest
-contamination = 0.04
-model = IsolationForest(contamination=contamination, max_features=x_train.shape[1])
+# contamination = 0.06
+# model = IsolationForest(contamination=contamination, max_features=x_train.shape[1])
 
-#Training the model with train data
-model.fit(x_train.to_numpy())
-anomaly_score_training = model.decision_function(x_train.to_numpy())
-training_prediction = model.predict(x_train.to_numpy())
-training_result = np.add(training_prediction,y_train)
-
-
-#Plotting training heatmap
-anomaly_train_plot = plt.scatter(x_train['rpm'], x_train['speed'], c = -1*anomaly_score_training, cmap = 'coolwarm')
-plt.colorbar(anomaly_train_plot,label = 'More Red = More Anomalous')
-plt.xlabel('RPM', fontsize = 12)
-plt.ylabel('Speed', fontsize = 12)
-plt.grid()
-plt.savefig(f"heatmap_train_contamination_{contamination}.png")
-plt.clf()
-
-#Plotting training prediction
-prediction_plot = plt.scatter(x_train['rpm'], x_train['speed'], c = -1*training_prediction, cmap = 'coolwarm')
-plt.colorbar(prediction_plot,label = 'More Red = More Anomalous')
-plt.xlabel('RPM', fontsize = 14)
-plt.ylabel('Speed', fontsize = 14)
-plt.grid()
-plt.title(f'Contamination = {contamination}, Accuracy ={round(list(training_result).count(0)/len(training_result),3)}', weight = 'bold')
-plt.savefig(f"prediction_train_contamination_{contamination}.png")
-plt.clf()
-
-#Testing the accuracy of the model on data unseen before
-anomaly_score_test = model.decision_function(x_test.to_numpy())
-test_prediction = model.predict(x_test.to_numpy())
-test_result = np.add(test_prediction,y_test)
-
-#Plotting test heatmap
-anomaly_test_plot = plt.scatter(x_test['rpm'], x_test['speed'], c = -1*anomaly_score_test, cmap = 'coolwarm')
-plt.colorbar(anomaly_test_plot,label = 'More Red = More Anomalous')
-plt.xlabel('RPM', fontsize = 12)
-plt.ylabel('Speed', fontsize = 12)
-plt.grid()
-plt.savefig(f"heatmap_test_contamination_{contamination}.png")
-plt.clf()
-
-#Plotting test prediction
-test_prediction_plot = plt.scatter(x_test['rpm'], x_test['speed'], c = -1*test_prediction, cmap = 'coolwarm')
-plt.colorbar(test_prediction_plot,label = 'More Red = More Anomalous')
-plt.xlabel('RPM', fontsize = 14)
-plt.ylabel('Speed', fontsize = 14)
-plt.grid()
-plt.title(f'Contamination = {contamination}, Accuracy ={round(list(test_result).count(0)/len(test_result),3)}', weight = 'bold')
-plt.savefig(f"prediction_test_contamination_{contamination}.png")
-plt.clf()
+# #Training the model with train data
+# model.fit(x_train.to_numpy())
+# anomaly_score_training = model.decision_function(x_train.to_numpy())
+# training_prediction = model.predict(x_train.to_numpy())
 
 
-# Get Anomaly Scores and Predictions
-#anomaly_score = model.decision_function(complete_np_dataframe)
-#predictions = model.predict(complete_np_dataframe)
-#res = np.add(predictions,rpm_speed_df["anomaly"])
-
-# #Plotting result
-# anomaly_score = -1*anomaly_score
-# anomaly_plot = plt.scatter(complete_dataframe['rpm'], complete_dataframe['speed'], c = anomaly_score, cmap = 'coolwarm')
-# plt.colorbar(anomaly_plot,label = 'More Red = More Anomalous')
+# #Plotting training heatmap
+# anomaly_train_plot = plt.scatter(x_train['rpm'], x_train['speed'], c = -1*anomaly_score_training, cmap = 'coolwarm')
+# plt.colorbar(anomaly_train_plot,label = 'More Red = More Anomalous')
 # plt.xlabel('RPM', fontsize = 12)
 # plt.ylabel('Speed', fontsize = 12)
 # plt.grid()
-# plt.savefig("heatmap.png")
+# plt.savefig(f"heatmap_train_contamination_{contamination}.png")
+# plt.clf()
 
-# prediction_plot = plt.scatter(complete_dataframe['rpm'], complete_dataframe['speed'], c = predictions, cmap = 'coolwarm')
+# #Plotting training prediction
+# prediction_plot = plt.scatter(x_train['rpm'], x_train['speed'], c = -1*training_prediction, cmap = 'coolwarm')
+# plt.colorbar(prediction_plot,label = 'More Red = More Anomalous')
 # plt.xlabel('RPM', fontsize = 14)
 # plt.ylabel('Speed', fontsize = 14)
 # plt.grid()
-# plt.title(f'Contamination = {contamination}, Accuracy ={round(list(res).count(0)/len(res),3)}', weight = 'bold')
-# plt.savefig("prediction.png")
+# plt.title(f'Contamination = {contamination}', weight = 'bold')
+# plt.savefig(f"prediction_train_contamination_{contamination}.png")
+# plt.clf()
 
-# print(f"anomalies = {list(predictions).count(-1)}")
-# print(f"accuracy = {list(res).count(0)/len(res)}")
+# #Testing the accuracy of the model on data unseen before
+# anomaly_score_test = model.decision_function(x_test.to_numpy())
+# test_prediction = model.predict(x_test.to_numpy())
+
+# #Plotting test heatmap
+# anomaly_test_plot = plt.scatter(x_test['rpm'], x_test['speed'], c = -1*anomaly_score_test, cmap = 'coolwarm')
+# plt.colorbar(anomaly_test_plot,label = 'More Red = More Anomalous')
+# plt.xlabel('RPM', fontsize = 12)
+# plt.ylabel('Speed', fontsize = 12)
+# plt.grid()
+# plt.savefig(f"heatmap_test_contamination_{contamination}.png")
+# plt.clf()
+
+# #Plotting test prediction
+# test_prediction_plot = plt.scatter(x_test['rpm'], x_test['speed'], c = -1*test_prediction, cmap = 'coolwarm')
+# plt.colorbar(test_prediction_plot,label = 'More Red = More Anomalous')
+# plt.xlabel('RPM', fontsize = 14)
+# plt.ylabel('Speed', fontsize = 14)
+# plt.grid()
+# plt.title(f'Contamination = {contamination}', weight = 'bold')
+# plt.savefig(f"prediction_test_contamination_{contamination}.png")
+# plt.clf()
+
+# #Evaluating accuracy of the model
+
+# #In isolation forest -1 is anomalous and 1 is not anomalous
+# training_prediction = [0 if i == 1 else 1 for i in training_prediction]
+# test_prediction = [0 if i == 1 else 1 for i in test_prediction]
+
+
+# #Plotting the result
+# columns = ["Predicted not anomalous","Predicted anomalous"]
+# rows = ["Actual not anomalous","Actual anomalous"]
+# training_confusion_matrix = pd.DataFrame(confusion_matrix(y_true=y_train,y_pred=training_prediction),columns=columns,index=rows)
+# sns.heatmap(training_confusion_matrix,annot=True,fmt='d',cmap="gist_gray_r",cbar=False).set(title="Training result")
+# plt.savefig(f"training_confusion_matrix_{contamination}.png")
+# plt.clf()
+# testing_confusion_matrix = pd.DataFrame(confusion_matrix(y_true=y_test,y_pred=test_prediction),columns=columns,index=rows)
+# sns.heatmap(testing_confusion_matrix,annot=True,fmt='d',cmap="gist_gray_r",cbar=False).set(title="Testing result")
+# plt.savefig(f"testing_confusion_matrix_{contamination}.png")
+
+
+
+
+
+
+
+
+"""
+https://github.com/facebookresearch/Kats
+"""
--- Machine Learning/test.py
+++ Machine Learning/test.py
@@ -1,3 +1,100 @@
+# with open("rpm.csv","r") as f:
+#     with open("rpmAnomaly.csv","w") as d:
+#         i = 0
+#         for line in f:
+#             if i == 0:
+#                 d.write(line.strip() + ",anomaly\n")
+#             else:
+#                 d.write(line.strip() + ",0\n")
+#             i+=1
+from cProfile import label
+import pandas as pd
+import matplotlib.pyplot as plt
 import numpy as np
+from random import randrange
+import random
+import datetime
+from sklearn.ensemble import IsolationForest
+from sklearn.model_selection import train_test_split
 
-print(np.arange(0.01,0.07,0.01))
+seed = 3
+
+random.seed(seed)
+def random_date(start,l):
+    dates = []
+    current = start
+    while l > 0:
+        current = current + datetime.timedelta(minutes=randrange(300))
+        while current in dates:
+            current = current + datetime.timedelta(minutes=randrange(300))
+        dates.append(current)
+        l-=1
+    return dates
+
+#pre-processing speed data
+speed_df = pd.read_csv('speedAnomaly.csv')
+speed_df = speed_df.drop(['id', 'vehicle_id','sensor'], axis=1)
+speed_df = speed_df.rename(columns={"data" : "speed"})
+print(speed_df.head())
+#pre-processing rpm data
+rpm_df = pd.read_csv('rpmAnomaly.csv')
+rpm_df = rpm_df.drop(['id', 'vehicle_id','sensor'], axis=1)
+rpm_df = rpm_df.rename(columns={"data" : "rpm"})
+print(rpm_df.head())
+
+timestamp = rpm_df["timestamp"]
+print(timestamp.head())
+
+low_rpm_high_speed = []
+high_rpm_low_speed = []
+
+i = 0
+i1 = 0
+while i < 75:
+    index = random.randint(0,timestamp.shape[0] - 1)
+    while index in low_rpm_high_speed:
+        index = random.randint(0,timestamp.shape[0] - 1)
+    low_rpm_high_speed.append(index)
+    i+=1
+while i1 < 75:
+    index = random.randint(0,timestamp.shape[0] - 1)
+    while index in high_rpm_low_speed or index in low_rpm_high_speed:
+        index = random.randint(0,timestamp.shape[0] - 1)
+    high_rpm_low_speed.append(index)
+    i1+=1
+print(len(high_rpm_low_speed))
+print(len(low_rpm_high_speed))
+
+print(rpm_df.iloc[high_rpm_low_speed[0]], high_rpm_low_speed[0])
+for i in high_rpm_low_speed:
+    rpm_df.loc[i,"rpm"] = random.randint(2500,4000)
+    speed_df.loc[i,"speed"] = random.randint(0,40)
+    rpm_df.loc[i,"anomaly"] = 1
+    speed_df.loc[i,"anomaly"] = 1
+for i in low_rpm_high_speed:
+    rpm_df.loc[i,"rpm"] = random.randint(0,1000)
+    speed_df.loc[i,"speed"] = random.randint(80,150)
+    rpm_df.loc[i,"anomaly"] = 1
+    speed_df.loc[i,"anomaly"] = 1
+
+rpm_df.to_csv("rpmAn.csv")
+speed_df.to_csv("speedAn.csv")
+
+
+#joining rpm and speed dataframe on timestamp
+#columns_names = ["timestamp","speed","rpm"]
+#labels = []
+#for i in range(rpm_df.shape[0]):
+    #labels.append(rpm_df["anomaly"][i] or speed_df["anomaly"][i])
+#rpm_speed_df = pd.merge(rpm_df,speed_df,on="timestamp")
+#rpm_speed_df = rpm_speed_df.reindex(columns=columns_names)
+#rpm_speed_df = pd.concat([rpm_speed_df,pd.DataFrame({"is_anomalous":labels})],axis=1)
+#print(rpm_speed_df.head())
+#print(rpm_speed_df.head(5))
+# label_df = pd.DataFrame({"anomaly" : [-1]*rpm_speed_df.shape[0]})
+# rpm_speed_df = pd.concat([rpm_speed_df,label_df],axis=1)
+#ax = plt.gca()
+#ax.set_xlim([-100, 4500])
+#plt.scatter(rpm_speed_df["rpm"],rpm_speed_df["speed"],c=rpm_speed_df["anomaly"],cmap="coolwarm")
+#plt.savefig("rpm_speed_scatter.png")
+#plt.clf()
